631bc5abc79620d918896b82993ca239d13cf113
Jakub Dudziński <jdudzinski101@gmail.com>
2024-04-21 13:17:35 +0200

    próbne ścieżki dla przepisów

--- backend/app.py
+++ backend/app.py
@@ -18,11 +18,30 @@ def home():
         "Message": "app up and running successfully"
         })
 
-@app.route("/make_table")
-def make_table():
-    return jsonify({
-        "Message": "ta funkcjonalnosc juz nie jest obslugiwana"
-        })
+@app.route("/przepisy")
+def fetch_all_przepisy():
+    query = "select id, nazwa, opis, poziom_trudnosci, kalorycznosc from przepis;"
+    cur.execute(query)
+    przepisy = cur.fetchall()
+    return jsonify(przepisy)
+
+@app.route("/przepisy/<int:id>", methods=['GET', 'POST', 'DELETE'])
+def fetch_przepis_with_id(id):
+    if request.method == 'GET':
+        query = f"select * from przepis where id = {id};"
+        cur.execute(query)
+        przepis = cur.fetchone()
+        return jsonify(przepis)
+    elif request.method == 'POST':
+        dane = request.json
+        return jsonify(
+            {
+                "Message": "probowałeś update'ować przepis",
+                "Twoje nowe hasło": request.form.get("nowe_haslo")
+            }
+        )
+    else:
+        return jsonify({"Message": "nie obsługiwana metoda"})
 
 
 if __name__=="__main__":
