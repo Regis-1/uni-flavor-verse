ab38e96bad540cebcc257fc8977215d071e280ee
Regis-1 <jdudzinski101@gmail.com>
2024-04-22 23:33:25 +0200

    simple POST method handling

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,5 @@
 from flask import Flask,jsonify,request
 
-import os
 import psycopg2
 
 conn = psycopg2.connect(
@@ -12,36 +11,81 @@ cur = conn.cursor()
 
 app = Flask(__name__)
 
+def create_przepis_from_json(json_data):
+    if not json_data:
+        return {"Message": "Błędny format JSON!"}
+
+    #checking for all necessary fields
+    #id?, autor, nazwa, poziom_trudnosci, procedura_wykonania
+    necessary_fields = ["autor", "nazwa", "poziom_trudnosci", "procedura_wykonania"]
+    fields = {
+            "autor": None,
+            "nazwa": None,
+            "opis": None,
+            "poziom_trudnosci": None,
+            "procedura_wykonania": None,
+            "kalorycznosc": None
+    }
+    for k, v in fields.items():
+        v = json_data.get(k)
+        fields[k] = v
+        if (not v) and (k in necessary_fields):
+            return {"Message": f"Brakujące dane do stworzenia nowego przepisu: {k}"}
+    
+    #query builder
+    query_elements = (
+            'insert into Przepis(autor,nazwa,poziom_trudnosci,procedura_wykonania',
+            '' if not fields['opis'] else ',opis',
+            '' if not fields['kalorycznosc'] else ',kalorycznosc',
+            ') values (',
+            "{}, '{}', {}, '{}'".format(fields['autor'],fields['nazwa'],
+                 fields['poziom_trudnosci'],fields['procedura_wykonania']),
+            '' if not fields['opis'] else ",'{}'".format(fields['opis']),
+            '' if not fields['kalorycznosc'] else ",{}".format(fields['kalorycznosc']),
+            ');')
+
+    query = ''.join(query_elements)
+    
+    cur.execute(query)
+    conn.commit()
+    return {
+            "Message": "Przepis został poprawnie dodany",
+            "Query": query
+            } 
+
+
 @app.route("/")
 def home():
     return jsonify({
-        "Message": "app up and running successfully"
+        "Message": "Witamy REST API flavor-verse - internetowej bazy danych z przepisami kulinarnymi"
         })
 
-@app.route("/przepisy")
+
+@app.route("/api/przepisy/", methods=['GET'])
 def fetch_all_przepisy():
     query = "select id, nazwa, opis, poziom_trudnosci, kalorycznosc from przepis;"
     cur.execute(query)
     przepisy = cur.fetchall()
     return jsonify(przepisy)
 
-@app.route("/przepisy/<int:id>", methods=['GET', 'POST', 'DELETE'])
+
+@app.route("/api/przepisy/<int:id>", methods=['GET'])
 def fetch_przepis_with_id(id):
-    if request.method == 'GET':
-        query = f"select * from przepis where id = {id};"
-        cur.execute(query)
-        przepis = cur.fetchone()
-        return jsonify(przepis)
-    elif request.method == 'POST':
-        dane = request.json
-        return jsonify(
-            {
-                "Message": "probowałeś update'ować przepis",
-                "Twoje nowe hasło": request.form.get("nowe_haslo")
-            }
-        )
+    query = f"select * from przepis where id = {id};"
+    cur.execute(query)
+    przepis = cur.fetchone()
+    return jsonify(przepis)
+
+
+@app.route("/api/przepisy/", methods=['POST'])
+def create_new_przepis():
+    if not request.is_json:
+        return jsonify({
+            "Message": "Zły rodzaj żądania. Wymagany jest typ application/json."
+        })
     else:
-        return jsonify({"Message": "nie obsługiwana metoda"})
+        rd = create_przepis_from_json(request.get_json())
+        return jsonify(rd)
 
 
 if __name__=="__main__":
--- db/tables.sql
+++ db/tables.sql
@@ -8,6 +8,7 @@ create table Uzytkownik(
 -- PRZEPIS
 create table Przepis(
     id serial PRIMARY KEY, -- primary key
+    autor integer not null, -- foreign key
     nazwa varchar(128) not null,
     opis varchar(1024),
     poziom_trudnosci integer not null,
@@ -15,24 +16,14 @@ create table Przepis(
     kalorycznosc integer
 );
 
+alter table Przepis add constraint autor_fk
+    foreign key (autor) references Uzytkownik(id) on delete cascade;
+
 -- SKLADNIK
 create table Skladnik(
     id serial PRIMARY KEY, -- primary key
     nazwa varchar(128) not null
 );
-
--- UZYTKOWNIK_PRZEPISY
-create table Uzytkownik_przepisy(
-    id serial PRIMARY KEY, -- primary key
-    uzytkownik integer not null, -- foreign key
-    przepis integer not null -- foreign key
-);
-
-alter table Uzytkownik_przepisy add constraint uzytkownik_fk
-    foreign key (uzytkownik) references Uzytkownik(id) on delete cascade;
-
-alter table Uzytkownik_przepisy add constraint przepis_fk
-    foreign key (przepis) references Przepis(id) on delete cascade;
 
 -- PRZEPIS_SKLADNIKI
 create table Przepis_skladniki(
@@ -50,8 +41,7 @@ alter table Przepis_skladniki add constraint skladnik_fk
     foreign key (skladnik) references Skladnik(id) on delete cascade;
 
 -- LOADING DATA
-copy Uzytkownik from '/db_data/uzytkownicy.csv' with (format CSV, delimiter ',', quote '"', header true);
-copy Skladnik from '/db_data/skladniki.csv' with (format CSV, delimiter ',', quote '"', header true);
-copy Przepis from '/db_data/przepisy.csv' with (format CSV, delimiter ',', quote '"', header true);
-copy Uzytkownik_przepisy from '/db_data/uzytkownik_przepisy.csv' with (format CSV, delimiter ',', quote '"', header true);
-copy Przepis_skladniki from '/db_data/przepis_skladniki.csv' with (format CSV, delimiter ',', quote '"', header true);
+copy Uzytkownik(nazwa_uzytkownika, haslo) from '/db_data/uzytkownicy.csv' with (format CSV, delimiter ',', quote '"', header true);
+copy Skladnik(nazwa) from '/db_data/skladniki.csv' with (format CSV, delimiter ',', quote '"', header true);
+copy Przepis(autor,nazwa,opis,poziom_trudnosci,procedura_wykonania,kalorycznosc) from '/db_data/przepisy.csv' with (format CSV, delimiter ',', quote '"', header true);
+copy Przepis_skladniki(przepis,skladnik,ilosc,miara) from '/db_data/przepis_skladniki.csv' with (format CSV, delimiter ',', quote '"', header true);
