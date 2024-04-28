392a92e7462a2a90d01f23a32694a969214aa9c3
Regis-1 <jdudzinski101@gmail.com>
2024-04-28 18:51:34 +0200

    added POST method handling for Przepis_skladniki table

--- backend/routes/przepisy/page.py
+++ backend/routes/przepisy/page.py
@@ -1,6 +1,6 @@
 from flask import jsonify, request
 from db_utils import (create_from_json, read_columns_from_table, 
-    update_from_json, delete_record_from_table, cur)
+    update_from_json, delete_record_from_table, cur, conn)
 from . import bp_przepisy
 
 # get all przepisy from the database
@@ -61,11 +61,52 @@ def update_selected_przepis(id):
 # read all skladniki for specific przepis with id
 @bp_przepisy.route("/api/przepisy/skladniki/<int:id>", methods=['GET'])
 def fetch_all_skladniki_przepisu(id):
-    query = ("select s.id, s.nazwa as nazwa from Przepis_skladniki ps"
+    query = ("select s.id, s.nazwa, ps.ilosc, ps.miara as nazwa from Przepis_skladniki ps"
              " inner join Przepis p on ps.przepis = p.id"
              " inner join Skladnik s on ps.skladnik = s.id"
              f" where p.id = {id};")
     cur.execute(query)
     results = cur.fetchall()
-    results = [{"id":r[0], "nazwa":r[1]} for r in results]
+    results = [{"id":r[0], "nazwa":r[1], "ilosc":r[2], "miara":r[3]} for r in results]
     return jsonify(results)
+
+@bp_przepisy.route("/api/przepisy/skladniki/<int:id>", methods=['POST'])
+def create_przepis_skladniki(id):
+    if not request.is_json:
+        return jsonify({
+            "Message": "Zły rodzaj żądania. Wymagany jest typ application/json."
+        })
+    else:
+        result = request.get_json()
+        if type(result) is not list:
+            return jsonify({"Message": "Przesłane dane nie są umieszczone w liście."})
+        if len(result) == 0:
+            return jsonify({"Message": "Nie dodano żadnych rekordów."})
+        
+        # filter to leave only dictionaries
+        result = [x for x in result if type(x) is dict]
+
+        values = ""
+        for r in result:
+            skladnik = r.get('skladnik')
+            ilosc = r.get('ilosc')
+            miara = r.get('miara')
+            if not skladnik or not ilosc or not miara:
+                continue
+            if type(skladnik) is not int or type(ilosc) is not int or type(miara) is not str:
+                continue
+
+            values = values + f"({id}, {skladnik}, {ilosc}, '{miara}'),"
+
+        if len(values) == 0:
+            return jsonify({"Message": "Nie dodano żadnych rekordów."})
+
+        values = values[:-1] + ';'
+        query = f"insert into Przepis_skladniki (przepis, skladnik, ilosc, miara) values "
+        query = query + values
+        
+        cur.execute(query)
+        conn.commit()
+        return jsonify({
+            "Message": "Skladniki przepisu zostały poprawnie dodane."
+        })
